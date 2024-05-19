823a573162580ded6d4d2d08bce40a0aed55ab7c
Sonali Rana <ranasonali987@gmail.com>
2024-05-19 19:56:45 +0530

    deploy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let data = {};
 let connectedSocketIds = [];
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "*"
 }));
 app.use(express.static('dist'));
 
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173"
+    origin: "*"
 }
 });
 
