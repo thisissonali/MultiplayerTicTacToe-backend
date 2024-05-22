589f7cd1b3d4eddf53051cfd65bcb082edf9eeb3
Sonali Rana <ranasonali987@gmail.com>
2024-05-22 15:40:48 +0530

    feat: added scaling and play again logic

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const uuid4 = require('uuid4');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-let users = 0;
-let data = {};
+let data = {}; // will have room to user mapping
+let users = {}; // will have user to room mapping
 let connectedSocketIds = [];
 let newRoom;
+let userQueue = [];
+
 app.use(cors({
   origin: "*"
 }));
@@ -40,7 +42,6 @@ const gotWinner = (index, updatedGrid) => {
       updatedGrid[row * 3 + 1] === updatedGrid[row * 3 + 2] &&
       updatedGrid[row * 3] !== ""
     ) {
-      users = users - 2;
       return updatedGrid[row * 3];
     }
     if (
@@ -48,7 +49,6 @@ const gotWinner = (index, updatedGrid) => {
       updatedGrid[col + 3] === updatedGrid[col + 6] &&
       updatedGrid[col] !== ""
     ) {
-      users = users - 2;
       return updatedGrid[col];
     }
     if (diagnoal1) {
@@ -57,7 +57,6 @@ const gotWinner = (index, updatedGrid) => {
         updatedGrid[4] === updatedGrid[8] &&
         updatedGrid[0] !== ""
       ) {
-        users = users - 2;
         return updatedGrid[0];
       }
     }
@@ -67,13 +66,14 @@ const gotWinner = (index, updatedGrid) => {
         updatedGrid[4] === updatedGrid[6] &&
         updatedGrid[2] !== ""
       ) {
-        users = users - 2;
         return updatedGrid[2];
       }
     }
 };
 
-const clickHandler = (index) => {
+const clickHandler = (index, newRoom) => {
+  console.log(newRoom);
+  console.log(data[newRoom]);
    if (data[newRoom].gridVal[index] || data[newRoom].winner) {
         return;
     }
@@ -91,43 +91,81 @@ const clickHandler = (index) => {
 };
 
 io.on("connection", (socket) => {
-  if (users === 0) connectedSocketIds.splice(0, connectedSocketIds.length);
-    users++;
-    if (users % 2 !== 0) {
-      //odd number of users
+     userQueue.push(socket);
+    console.log(userQueue.length); 
+    if (userQueue.length >= 2) {
+      const socket1 = userQueue.shift();
+      const socket2 = userQueue.shift();
+      console.log("hey");
       newRoom = uuid4();
-      socket.join(newRoom);
-      connectedSocketIds.push(socket.id);
+      socket1.join(newRoom);
+      socket2.join(newRoom);
+      users[socket1.id] = newRoom;
+      users[socket2.id] = newRoom;
+
+      connectedSocketIds.push(socket1.id);
+      connectedSocketIds.push(socket2.id);
+      data[newRoom] = { gridVal: Array(9).fill(""), chance: true, connecIdsArr: connectedSocketIds, winner: '' }; 
+      console.log(connectedSocketIds);
+      io.to(users[socket.id]).emit("data", data, users[socket.id]);
     } else {
-      connectedSocketIds.push(socket.id);
-      socket.join(newRoom);
-      users = users - 2;
-      data[newRoom] = {gridVal:  Array(9).fill("") , chance: true , connecIdsArr: connectedSocketIds , winner: ''}; 
+      connectedSocketIds = [];
+      console.log(connectedSocketIds);
     }
-    data[newRoom] = {gridVal:  Array(9).fill("") , chance: true , connecIdsArr: connectedSocketIds , winner: ''}; 
-    io.to(newRoom).emit("data", data, newRoom);
    
-  socket.on("click-event", (index) => { 
-      if (data[newRoom].chance && socket.id === data[newRoom].connecIdsArr[0]) {
-       // X has chance
-        clickHandler(index);
-     }
-     else if (!data[newRoom].chance && socket.id === data[newRoom].connecIdsArr[1]) {
-       //0 has chance
-       clickHandler(index);
-    }  
-     io.to(newRoom).emit("data", data, newRoom);
-  })
-   socket.on("disconnect", () => {
-    users--;
-    data[newRoom].connecIdsArr = data[newRoom].connecIdsArr.filter(id => id !== socket.id);
-    if (data[newRoom].connecIdsArr.length === 0) {
-      // Reset game state if both players leave
-      data[newRoom].gridVal.fill("");
-      data[newRoom].winner = '';
-      data[newRoom].chance = true;
-    }
-  });
+  socket.on("click-event", (index) => {
+    console.log(data[users[socket.id]].chance);
+    console.log(data[users[socket.id]]);
+    console.log(socket.id);
+    console.log(data[users[socket.id]].connecIdsArr[0]);
+    if (data[users[socket.id]].chance && socket.id === data[users[socket.id]].connecIdsArr[0]) {
+      // X has chance
+      console.log('x clickhandler worked');
+      console.log(users);
+      console.log(users[socket.id]);
+      clickHandler(index, users[socket.id]);
+    }
+    else if (!data[users[socket.id]].chance && socket.id === data[users[socket.id]].connecIdsArr[1]) {
+      //0 has chance
+      console.log('0 clickhandler worked');
+      console.log(users);
+      console.log(users[socket.id]);
+      clickHandler(index, users[socket.id]);
+    }
+    io.to(users[socket.id]).emit("data", data, users[socket.id]);
+  });
+  socket.on("request-new-room", (userId) => {
+    const currentRoom = users[userId];
+    const newRoom = uuid4();
+    data[newRoom] = { gridVal: Array(9).fill(""), chance: true, connecIdsArr: [], winner: '' };
+
+    const userIndex = data[currentRoom].connecIdsArr.indexOf(userId);
+    if (userIndex >= 0) {
+      data[newRoom].connecIdsArr.push(userId);
+      socket.join(newRoom);
+      users[userId] = newRoom;
+
+      if (data[currentRoom].connecIdsArr.length === 2) {
+        const otherUserId = data[currentRoom].connecIdsArr[1 - userIndex];
+        data[newRoom].connecIdsArr.push(otherUserId);
+        io.sockets.sockets.get(otherUserId).leave(currentRoom);
+        io.sockets.sockets.get(otherUserId).join(newRoom);
+        users[otherUserId] = newRoom;
+      }
+    }
+
+    io.to(newRoom).emit("new-room", data[newRoom]);
+  });
+    socket.on("disconnect", () => {
+    const room = users[socket.id];
+    if (room && data[room]) {
+      data[room].connecIdsArr = data[room].connecIdsArr.filter(id => id !== socket.id);
+
+      if (data[room].connecIdsArr.length === 0) {
+        delete data[room];
+      }
+    }
+  });
 });
 app.get('/', async (req , res) => {
   res.sendFile(path.resolve(__dirname,  "dist", "index.html"));
